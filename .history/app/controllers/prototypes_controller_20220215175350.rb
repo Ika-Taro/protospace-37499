479c5f94c8ad74e8952d5791c8920a15ed08c48c
Ika-Taro <black.jkh@gmail.com>
2022-02-15 18:08:18 +0900

    プロトタイプの削除

--- .history/app/controllers/prototypes_controller_20220215175350.rb
+++ .history/app/controllers/prototypes_controller_20220215175350.rb
@@ -38,9 +38,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
-    redirect_to root_path
+    
   end
 
   private
